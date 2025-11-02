4eae43d111e3d83ab1a02bc59a0d0ff9fa51d68e
wesscav <wesleyalvescav@gmail.com>
2025-11-02 17:12:12 -0300

    feat: adição da tela inicial do projeto

---
+++ src/pages/_app.jsx
@@ -0,0 +1,19 @@
+import "../styles/globals.css";
+import AuthLayout from "@/components/AuthLayout";
+
+export default function MyApp({ Component, pageProps, router }) {
+  const isAuthRoute = router.pathname.startsWith("/auth");
+
+  if (isAuthRoute) {
+    return (
+      <AuthLayout>
+        <Component {...pageProps} />
+      </AuthLayout>
+    );
+  }
+
+  return <Component {...pageProps} />;
+}
+
+
+
