7e81f18276f50301f434005f9d585b8f7abdd38f
wesscav <wesleyalvescav@gmail.com>
2025-11-02 17:13:36 -0300

    feat: _document

---
+++ src/pages/_document.jsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet" />
+
+      <Head />
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+
+
