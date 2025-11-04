8738ea28bbef35aa4edb449c5ccd64fcce635015
Eduardo Marcelino <eduardoofficial12@gmail.com>
2025-11-04 18:11:33 -0300

    finalizado

--- src/pages/auth/layout.jsx
+++ src/pages/auth/layout.jsx
@@ -1,8 +1,5 @@
 import AuthLayout from "@/components/AuthLayout"; 
 
-export const metadata = {
-  title: "Entrar — Hub de Entidades",
-};
 
 export default function AuthPageLayout({ children }) {
   return <AuthLayout>{children}</AuthLayout>;
